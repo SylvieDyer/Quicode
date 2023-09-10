47156df9a02b94cc8b5d3a3af046e8a8da177756
Nivita <nnp25@case.edu>
2023-09-10 19:05:57 -0400

    added soto package dependency

---
+++ CSDS395/AWSManager.swift
@@ -0,0 +1,11 @@
+//
+//  AWSManager.swift
+//  CSDS395
+//
+//  Created by Nivita Patri on 9/10/23.
+//
+
+import Foundation
+import SotoS3
+
+
--- CSDS395/CSDS395App.swift
+++ CSDS395/CSDS395App.swift
@@ -17,3 +17,9 @@ struct CSDS395App: App {
         }
     }
 }
+
+struct Previews_CSDS395App_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
