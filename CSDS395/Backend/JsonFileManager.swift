1fbd25bd83c5c3334cf52c38219191b0cb878654
Kim Tran <kdt46@case.edu>
2023-09-11 23:35:12 -0400

    create user after login

---
+++ CSDS395/Backend/JsonFileManager.swift
@@ -0,0 +1,26 @@
+//
+//  JsonFileManager.swift
+//  CSDS395
+//
+//  Created by Tran Dac Nguyen Kim on 9/11/23.
+//
+
+import Foundation
+
+
+func writeJson(destPath: String, userData: User) {
+    // Create a JSONEncoder
+    let encoder = JSONEncoder()
+    print(URL(fileURLWithPath: FileManager.default.currentDirectoryPath))
+    do {
+        // Encode the UserData instance into JSON data
+        let jsonData = try encoder.encode(userData)
+        let userJSONURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent(destPath)
+        print(userJSONURL)
+        
+        // Write the JSON data to the file
+        try jsonData.write(to: userJSONURL, options: .atomic)
+    } catch {
+        print("Error encoding UserData to JSON: \(error)")
+    }
+}
---
+++ CSDS395/Backend/User.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  CSDS395
+//
+//  Created by Tran Dac Nguyen Kim on 9/11/23.
+//
+
+struct User: Codable {
+    var id: String
+    var email: String
+    var lastname: String
+    var firstname: String
+}
