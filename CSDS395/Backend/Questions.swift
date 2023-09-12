69f2a8ce3bb88917135ec369c023f9d885799239
Prarthana Gajjala <prarthanajg@live.com>
2023-09-12 12:28:06 -0400

    add question answers

--- CSDS395/Backend/Questions.swift
+++ CSDS395/Backend/Questions.swift
@@ -16,10 +16,12 @@ public class Question {
     let questionText: String
     let questionType: QuestionType
     let questionOptions: [String]
+    let questionAnswer: [String]
     
-    init(_ questionText: String, _ questionType: QuestionType, _ questionOptions: [String]) {
+    init(_ questionText: String, _ questionType: QuestionType, _ questionOptions: [String], _ questionAnswer:[String]) {
         self.questionText = questionText
         self.questionType = questionType
         self.questionOptions = questionOptions
+        self.questionAnswer = questionAnswer
     }
 }
