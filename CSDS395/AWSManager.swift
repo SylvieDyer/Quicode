3871dbffd3b7560c24b7bb192e3d184b25fa341c
Nivita <nnp25@case.edu>
2023-09-13 19:02:32 -0400

    fix

--- CSDS395/AWSManager.swift
+++ CSDS395/AWSManager.swift
@@ -8,24 +8,26 @@
 import Foundation
 import SotoS3
 
-let bucket = "quicode"
-
-let client = AWSClient(
-    credentialProvider: .static(accessKeyId: "AKIA2ARVCSNBIO4SS2HU", secretAccessKey: "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"),
-    httpClientProvider: .createNew
-)
-let s3 = S3(client: client, region: .useast2)
-
-
-let uploadRequest = S3.PutObjectRequest(
-    bucket: "quicode",
-    key: "example.png",
-    body: Data("Your file content here".utf8)
-)
-
-do {
-    try s3.putObject(uploadRequest).wait()
-    print("Object uploaded successfully!")
-} catch {
-    print("Error uploading object: \(error)")
+func main() {
+    
+    let bucket = "quicode"
+    
+    let client = AWSClient(
+        credentialProvider: .static(accessKeyId: "AKIA2ARVCSNBIO4SS2HU", secretAccessKey: "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"),
+        httpClientProvider: .createNew
+    )
+    let s3 = S3(client: client, region: .useast2)
+    
+    
+    let uploadRequest = S3.PutObjectRequest(
+        bucket: "quicode",
+        key: "example.png"
+    )
+    
+    do {
+        try s3.putObject(uploadRequest).wait()
+        print("Object uploaded successfully!")
+    } catch {
+        print("Error uploading object: \(error)")
+    }
 }
