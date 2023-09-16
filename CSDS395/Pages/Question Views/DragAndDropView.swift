b28592a7cde1d62528480cb7672f9e41cbbc8bb4
DyerSylvie <sylvierui1@gmail.com>
2023-09-15 22:54:40 -0400

    minor adjustment to DND

--- CSDS395/Pages/Question Views/DragAndDropView.swift
+++ CSDS395/Pages/Question Views/DragAndDropView.swift
@@ -9,76 +9,84 @@ import SwiftUI
 import DragAndDrop
 
 struct DragAndDropView: View {
-
+    
     
     var question: Question
     
     let questionTemp = ["A", "BLANK", "IS", "A", "BLANK"];
     let answers = ["Integer", "Type"]
     let options = ["Variable", "Cow", "Type", "Pencil", "Integer"]
-    let answerIDs = [UUID(), UUID()]
+    
+    // to make values mutable
+    @State var answer1 = "NULL"
+    @State var answer2 = "NULL"
     
     var body: some View {
         InteractiveDragDropContainer{
             VStack{
                 HStack{
+                    
                     // TODO: issue with for loop rendering: catagorizes drop views w/ the same status......
-                   
-//                    ForEach(questionTemp, id: \.self) { word in
-//                        if (word == "BLANK"){
-//                            DropView { dropInfo in
-//                                Text(dropInfo.didDrop ? "DROPPED" : "BLANK")
-//                                               .padding()
-//                                               .background{
-//                                                   dropInfo.isColliding ? Color.green : Color.red
-//                                               }
-//                                       }
-//                                       .onDragViewReceived { receivingViewID in
-//                                           Text(receivingViewID.uuidString)
-//                                       }
-//                        }
-//                        else {
-//                            Text(word)
-//                        }
-//                    }
                     
-                    // TODO: droppedInfo.did drop - can't tell WHAT was dropped there
+                    /* LOOP TO RENDER QUESTION AND BLANK DROPVIEWS : */
                     
+                    //                    ForEach(questionTemp, id: \.self) { word in
+                    //                        if (word == "BLANK"){
+                    //                            DropView { dropInfo in
+                    //                                Text(dropInfo.didDrop ? "DROPPED" : "BLANK")
+                    //                                               .padding()
+                    //                                               .background{
+                    //                                                   dropInfo.isColliding ? Color.green : Color.red
+                    //                                               }
+                    //                                       }
+                    //                                       .onDragViewReceived { receivingViewID in
+                    //                                           Text(receivingViewID.uuidString)
+                    //                                       }
+                    //                        }
+                    //                        else {
+                    //                            Text(word)
+                    //                        }
+                    //                    }
+                    
+                    
+                    // TODO: .onDragViewReceived doesn't properly update the answer fields
+                    
+                    /* HARD CODED RENDERING OF SENTENCE AND DROP VIEW */
                     Text("A")
-                    
                     DropView { dropInfo in
-                                   Text(dropInfo.didDrop ? "Dropped" : "BLANK")
-                                       .padding()
-                                       .background{
-                                           dropInfo.isColliding ? Color.purple : Color.gray
-                                       }
-                               }
-                               .onDragViewReceived { receivingViewID in
-                                   print(receivingViewID)
-                               }
+                        Text(dropInfo.didDrop ? answer1 : "BLANK")
+                            .padding()
+                            .background{
+                                dropInfo.isColliding ? Color.purple : Color.gray
+                            }
+                    }
+                    .onDragViewReceived { receivingViewID in
+                        print(receivingViewID)
+                        // TODO: doesn't update text
+                        answer1 = receivingViewID.uuidString
+                    }
                     Text("IS")
                     Text("A")
                     DropView { dropInfo in
-                                   Text(dropInfo.didDrop ? "Dropped" : "BLANK")
-                                       .padding()
-                                       .background{
-                                           dropInfo.isColliding ? Color.purple : Color.gray
-                                       }
-                               }
-                               .onDragViewReceived { receivingViewID in
-                                   print(receivingViewID)
-                               }
+                        Text(dropInfo.didDrop ? answer2 : "BLANK")
+                            .padding()
+                            .background{
+                                dropInfo.isColliding ? Color.purple : Color.gray
+                            }
+                    }
+                    .onDragViewReceived { receivingViewID in
+                        answer2 = receivingViewID.uuidString
+                    }
                 }
                 HStack{
                     ForEach(options, id: \.self) { option in
-                        DragView(id: UUID()) { dragInfo in
+                        DragView(id: question.getID()) { dragInfo in
                             Text(option)
                                 .padding()
                                 .background{
                                     dragInfo.isDragging ? Color.blue : Color.mint
                                 }
                         }
-                        
                     }
                 }
             }
