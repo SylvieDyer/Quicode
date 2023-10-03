32136e4dc83a83ef468e590e23f56f28d22116a7
Kim Tran <kdt46@case.edu>
2023-10-03 13:29:23 -0400

    fetch file from aws

--- CSDS395/AWSManager.swift
+++ CSDS395/AWSManager.swift
@@ -43,27 +43,60 @@ struct AWSManager {
             key: "fun.json"
         )
         
+        /// FOR FETCHING
+        
+//        do {
+//            let response = try await s3.getObject(input)
+//
+//            if let objectData = response.body {
+//                do {
+//                    let data = try? objectData.asString()
+//                    if let unwrappedValue = data {
+//                        print("\(unwrappedValue)")
+//                    } else {
+//                        // optionalValue is nil
+//                        print("The optional is nil")
+//                    }
+//                } catch {
+//                    print("Error converting object data to Data: \(error)")
+//                }
+//            } else {
+//                print("No object data received.")
+//            }
+//            
+//            do {
+//                try await s3.client.shutdown()
+//            }
+//            catch {
+//                print("Error shutting down")
+//            }
+//        } catch {
+//            // Handle any errors that occur
+//            print("Error downloading object: \(error)")
+//        }
+
+        
       
-        do {
-            let output = try await s3.getObject(input)
-            guard let body = output.body,
-                  let data = try await body.asData()
-            else {
-                return "".data(using: .utf8)!
-            }
-            
-            do {
-                try await s3.client.shutdown()
-            }
-            catch {
-                print("Error shutting down")
-            }
-            
-            return data
-        }
-        catch {
-            print("error getting")
-        }
+//        do {
+//            let output = try await s3.getObject(input)
+//            guard let body = output.body,
+//                  let data = try await body.asData()
+//            else {
+//                return "".data(using: .utf8)!
+//            }
+//
+//            do {
+//                try await s3.client.shutdown()
+//            }
+//            catch {
+//                print("Error shutting down")
+//            }
+//
+//            return data
+//        }
+//        catch {
+//            print("error getting")
+//        }
     
         return Data()
         
