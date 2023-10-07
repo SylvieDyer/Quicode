06c0f66aefb34f3a389273a2e194b5446c001008
Helio <lxd262@case.edu>
2023-10-07 17:51:19 -0400

    Trying to remove user from Core Data after logout
    unsuccessful attempt

--- CSDS395/Pages/LoginView.swift
+++ CSDS395/Pages/LoginView.swift
@@ -61,14 +61,14 @@ struct LoginView: View {
         }
     }
     
-    // for sign in functional
+    // for sign in with Apple
     func SignInButton(_ type: SignInWithAppleButton.Style) -> some View{
         return SignInWithAppleButton(.signIn) { request in
             request.requestedScopes = [.fullName, .email]
         } onCompletion: { result in
             switch result {
             case .success(let authResults):
-                print("Authorisation successful \(authResults)")
+                print("Authorization successful \(authResults)")
                 // creates the user object
                 let user = CreateUser(authResults: authResults)
                 Task{
