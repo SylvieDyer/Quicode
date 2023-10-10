092289db41523bc15aad08a9c1269265913eb968
Helio <lxd262@case.edu>
2023-10-09 23:43:33 -0400

    idk why user is not being deleted from Core Data
    I might need some help tomorrow :)

---
+++ CSDS395/Pages/LogoutView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoutView.swift
+//  CSDS395
+//
+//  Created by Helio Dong on 10/9/23.
+//
+
+import SwiftUI
+
+struct LogoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoutView()
+}
