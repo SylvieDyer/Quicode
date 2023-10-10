fef22ccd15d22d40119f6926d14fd5cb3625b6e6
Nivita <nnp25@case.edu>
2023-10-10 10:18:48 -0400

    in-progress jsondecoder

--- CSDS395/Backend/JsonFileManager.swift
+++ CSDS395/Backend/JsonFileManager.swift
@@ -25,3 +25,70 @@ func writeJson<T: Encodable>(destPath: String, data: T) {
         print("Error encoding object to JSON: \(error)")
     }
 }
+
+import Foundation
+
+func pullJson(from filePath: String) -> QuestionList? {
+    // Read the JSON data from the file
+    guard let data = try? Data(contentsOf: URL(fileURLWithPath: filePath)) else {
+        return nil
+    }
+    
+    // Decode the JSON data into an array of dictionaries
+    guard let jsonFile = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
+        return nil
+    }
+    
+    // Initialize an empty array to hold Question objects
+    var questionList: [Question] = []
+    
+    // Iterate through the JSON array and create Question objects
+    for questionData in jsonFile {
+        if let questionTypeRaw = questionData["questionType"] as? String,
+           let questionText = questionData["questionText"] as? String,
+           let questionOptions = questionData["questionOptions"] as? [String],
+           let questionAnswer = questionData["questionAnswer"] as? [String] {
+            
+            let questionType: QuestionType
+            switch questionTypeRaw {
+            case "multiSelect":
+                questionType = .multiSelect
+            case "multipleChoice":
+                questionType = .multipleChoice
+            case "dragAndDrop":
+                questionType = .dragAndDrop
+            default:
+                questionType = .blank
+            }
+            
+            let question: Question
+            if questionType == .multiSelect {
+                question = MultipleQ(questionText: questionText, questionOptions: questionOptions, questionAnswer: questionAnswer)
+            } else {
+                question = BlankQ()
+            }
+            
+            questionList.append(question)
+        }
+    }
+    
+    // Create a QuestionList object with the parsed questions
+    let questionListObject = QuestionList(qlist: questionList)
+    
+    return questionListObject
+}
+
+
+    // Call pullJson for testing within the main function
+    if let questionList = pullJson(from: "sample.json") {
+        // Successfully parsed the JSON file into a QuestionList object.
+        // You can now work with the questions using methods from the QuestionList class.
+        // For example, you can print the first question's text:
+        let firstQuestion = questionList.getCurrent()
+        print("First question: \(firstQuestion.questionText)")
+    } else {
+        print("Failed to parse the JSON file.")
+    }
+
+
+
--- CSDS395/Backend/Questions.swift
+++ CSDS395/Backend/Questions.swift
@@ -10,25 +10,109 @@ public enum QuestionType {
     case multiSelect
     case multipleChoice
     case dragAndDrop
+    case blank
 }
 
-public class Question {
-    let questionText: String
-    let questionType: QuestionType
-    let questionOptions: [String]
-    let questionAnswer: [String]
-    let nextQuestion: Question? //LinkedList type structure
+protocol Question {
+    var questionType: QuestionType { get }
+    var questionText: String { get set }
+    var questionOptions: [String] { get set }
+    var questionAnswer: [String] { get set }
+    var id: UUID { get }
+}
+
+public class MultipleQ: Question {
+    var questionType: QuestionType
+    var questionText: String
+    var questionOptions: [String]
+    var questionAnswer: [String]
+    var id: UUID
     
-    init(_ questionText: String, _ questionType: QuestionType, _ questionOptions: [String], _ questionAnswer:[String], _ nextQuestion: Question? = nil) {
+    
+    init(questionText: String, questionOptions: [String], questionAnswer:[String]) {
+        self.questionType = QuestionType.multiSelect
         self.questionText = questionText
-        self.questionType = questionType
         self.questionOptions = questionOptions
         self.questionAnswer = questionAnswer
-        self.nextQuestion = nextQuestion
+        self.id = UUID()
     }
     
     func getID() -> UUID {
-        return UUID()
+        return self.id
+    }
+}
+
+public class BlankQ: Question {
+    var questionType: QuestionType
+    var questionText: String = "UNSET"
+    var questionOptions: [String] = []
+    var questionAnswer: [String] = []
+    var id: UUID
+    
+    init() {
+        self.questionType = QuestionType.blank
+        self.id = UUID()
     }
     
+    func getId() -> UUID {
+        return self.id
+    }
+}
+
+public class QuestionList {
+    var currentPos: Int
+    var qlist: [Question]
+    
+    init(qlist: [Question]) {
+        self.currentPos = 0
+        self.qlist = qlist
+    }
+    
+    func getCurrent() -> Question {
+        print("get current: " + String(currentPos))
+        return qlist[currentPos]
+    }
+    
+    // Returns nil if at last question
+    func getNext() -> Question? {
+        print(currentPos)
+        if (currentPos < qlist.count - 1) {
+            currentPos += 1
+            return getCurrent()
+        }
+        return nil
+    }
+    
+    func peekNext() -> Question? {
+        return getQuestionAtIndex(index: currentPos + 1)
+    }
+    
+    // Returns nil if at first question
+    func getLast() -> Question? {
+        if (currentPos > 0) {
+            currentPos -= 1
+            return getCurrent()
+        }
+        return nil
+    }
+    
+    // Returns nil if at first question
+    func peekLast() -> Question? {
+        return getQuestionAtIndex(index: currentPos - 1)
+    }
+    
+    func isLast() -> Bool {
+        return currentPos >= qlist.count - 1
+    }
+    
+    func getQuestionAtIndex(index: Int) -> Question? {
+        if (index < qlist.count && index >= 0) {
+            return qlist[index]
+        }
+        return nil
+    }
+    
+    func getRandomQuestion() -> Question? {
+        return getQuestionAtIndex(index: Int.random(in: 0..<qlist.count))
+    }
 }
