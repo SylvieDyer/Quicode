ed1f51ed7ddf187beeb78e0c3a2eb91180818d42
Prarthana Gajjala <prarthanajg@live.com>
2023-12-02 10:34:04 -0500

    ProgressUtils skelelton

---
+++ CSDS395/Backend/ProgressUtils.swift
@@ -0,0 +1,16 @@
+//
+//  ProgressUtils.swift
+//  CSDS395
+//
+//  Created by Prarthana Gajjala on 12/2/23.
+//
+
+import Foundation
+
+struct ProgressUtils {
+    @available(*, unavailable) private init() {}
+
+    static func getKey() -> String {
+        //
+    }
+}
