244caa83a22eaf321c7e378af267161fea373cbf
Nivita <nnp25@case.edu>
2023-12-02 17:53:46 -0500

    changed dnd height

--- CSDS395/Pages/Question Views/DragAndDropView.swift
+++ CSDS395/Pages/Question Views/DragAndDropView.swift
@@ -42,7 +42,7 @@ struct DragAndDropView: View{
                 .foregroundColor(colorManager.getMidGreen()
                     .opacity(isShown ? 1.0 : 0.0))
                     .padding(50)
-                    .frame(width:400, height: 300))
+                    .frame(width:400, height: 500))
             
             Spacer()
             
